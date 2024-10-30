5f0081467d95c32ca09ca0006b55ab4eeaffddca
Your Name <you@example.com>
2024-10-30 20:49:37 +0530

    new

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { EmployeComponent } from './pages/employe/employe.component';
+import { EmpLeaveComponent } from './pages/emp-leave/emp-leave.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'login',
+        pathMatch:'full'
+    },
+    {
+        path:'login',
+        component:LoginComponent
+    },
+    {
+        path:'',
+        component:LayoutComponent,
+        children:[
+            {
+                path:'dashboard',
+                component:DashboardComponent
+            },
+            {
+                path:'employee',
+                component:EmployeComponent
+            },
+            {
+                path:'leave-request',
+                component:EmpLeaveComponent
+            }
+        ]
+    }
+];
---
+++ src/app/pages/emp-leave/emp-leave.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emp-leave',
+  standalone: true,
+  imports: [],
+  templateUrl: './emp-leave.component.html',
+  styleUrl: './emp-leave.component.css'
+})
+export class EmpLeaveComponent {
+
+}
